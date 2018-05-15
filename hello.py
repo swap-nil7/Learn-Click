36138f3e33fb506224d00d12e8abf4271fdd6da6
Swapnil Negi <swapnil.negi09@gmail.com>
2018-05-15 18:58:06 +0530

    use helper class

--- hello.py
+++ hello.py
@@ -1,8 +1,16 @@
 import click
 
+class Config(object):
+    def __init__(self):
+        self.verbose = False
+
+pass_config = click.make_pass_decorator(Config, ensure=True)
+
 @click.group()
-def cli():
-    pass
+@click.option('--verbose', is_flag=True)
+@pass_config
+def cli(config, verbose):
+    config.verbose = verbose
 
 @cli.command()
 @click.option('--string', default='World',
@@ -11,8 +19,11 @@ def cli():
               help='The number of times to be greeted')
 @click.argument('out', type=click.File('w'), default='-',
                 required=False)
-def greet(string, repeat, out):
+@pass_config
+def greet(config, string, repeat, out):
     '''This is a script to greet'''
+    if config.verbose:
+        click.echo('swap_it')
     for i in xrange(repeat):
         click.echo('Hello {}'.format(string), file=out)
 
