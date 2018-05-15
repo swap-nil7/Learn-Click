722c3ec745369e44d52871304304e825a25cca59
Swapnil Negi <swapnil.negi09@gmail.com>
2018-05-15 18:28:58 +0530

    use arguments

--- hello.py
+++ hello.py
@@ -5,8 +5,10 @@ import click
               help='This is the thing that is greeted')
 @click.option('--repeat', default=1,
               help='The number of times to be greeted')
-def cli(string, repeat):
+@click.argument('out', type=click.File('w'), default='-',
+                required=False)
+def cli(string, repeat, out):
     '''This is a script to greet'''
     for i in xrange(repeat):
-        click.echo('Hello {}'.format(string))
+        click.echo('Hello {}'.format(string), file=out)
 
