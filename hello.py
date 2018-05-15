2be31d36038748ff5a716f5119b1f67a3e753b4c
Swapnil Negi <swapnil.negi09@gmail.com>
2018-05-15 18:35:38 +0530

    use groups for subcommands

--- hello.py
+++ hello.py
@@ -1,13 +1,17 @@
 import click
 
-@click.command()
+@click.group()
+def cli():
+    pass
+
+@cli.command()
 @click.option('--string', default='World',
               help='This is the thing that is greeted')
 @click.option('--repeat', default=1,
               help='The number of times to be greeted')
 @click.argument('out', type=click.File('w'), default='-',
                 required=False)
-def cli(string, repeat, out):
+def greet(string, repeat, out):
     '''This is a script to greet'''
     for i in xrange(repeat):
         click.echo('Hello {}'.format(string), file=out)
